780d0661a3ff70127ce670e1a83479e737319d74
PCDotFan <467055732@qq.com>
2020-11-22 19:08:18 +0800

    cleanup

--- src/chain.ts
+++ src/chain.ts
@@ -1,73 +1,83 @@
-#!/usr/bin/env node
-
 import { IPluginContext } from '@tarojs/service';
-import { HIDDEN_CONFIG_PATH, CURRENT_PLATFORM, SUPPORTED_MINI_PLATFORMS, SUPPORTED_PLATFORMS } from './constant';
+import {
+    HIDDEN_CONFIG_PATH,
+    CURRENT_PLATFORM,
+    SUPPORTED_MINI_PLATFORMS,
+    SUPPORTED_PLATFORMS,
+} from './constant';
 
 const fs = require('fs-extra');
 const path = require('path');
 
-const getConfigPath = (platform) => `${HIDDEN_CONFIG_PATH}/${platform}.config.js`;
-const checkConfigExists = (platform) => fs.existsSync(path.resolve(getConfigPath(platform)));
+const getConfigPath = platform => `${HIDDEN_CONFIG_PATH}/${platform}.config.js`;
+const checkConfigExists = platform => fs.existsSync(path.resolve(getConfigPath(platform)));
 
 export default (ctx: IPluginContext, test) => {
-  ctx.modifyWebpackChain(({ chain }) => {
-    if (!SUPPORTED_PLATFORMS.includes(CURRENT_PLATFORM)) {
-      console.log(
-        ctx.helper.chalk.yellowBright(
-          `⚠️ [taro-plugin-tailwind]: platform ${CURRENT_PLATFORM} is not supported, auto skipping...`,
-        ),
-      );
-      return;
-    }
-    if (!['h5', 'mini'].some((platform) => checkConfigExists(platform))) {
-      console.log(
-        ctx.helper.chalk.yellowBright(
-          `⚠️ [taro-plugin-tailwind]: required config (h5.config.js / mini.config.js) is missing, auto skipping...`,
-        ),
-      );
-      return;
-    }
-    const postcssConfig = {
-      mini: {
-        plugins: [
-          ['postcss-import', {}],
-          [
-            'tailwindcss',
-            {
-              config: getConfigPath(checkConfigExists(CURRENT_PLATFORM) ? CURRENT_PLATFORM : 'mini'),
+    ctx.modifyWebpackChain(({ chain }) => {
+        if (!SUPPORTED_PLATFORMS.includes(CURRENT_PLATFORM)) {
+            console.log(
+                ctx.helper.chalk.yellowBright(
+                    `⚠️ [taro-plugin-tailwind]: platform ${CURRENT_PLATFORM} is not supported, auto skipping...`
+                )
+            );
+            return;
+        }
+        if (!['h5', 'mini'].some(platform => checkConfigExists(platform))) {
+            console.log(
+                ctx.helper.chalk.yellowBright(
+                    `⚠️ [taro-plugin-tailwind]: required config (h5.config.js / mini.config.js) is missing, auto skipping...`
+                )
+            );
+            return;
+        }
+        const postcssConfig = {
+            mini: {
+                plugins: [
+                    ['postcss-import', {}],
+                    [
+                        'tailwindcss',
+                        {
+                            config: getConfigPath(
+                                checkConfigExists(CURRENT_PLATFORM) ? CURRENT_PLATFORM : 'mini'
+                            ),
+                        },
+                    ],
+                    ['postcss-discard-empty', {}],
+                    ['postcss-unprefix', {}],
+                    ['postcss-css-variables', {}],
+                    ['postcss-preset-env', {}],
+                ],
             },
-          ],
-          ['postcss-discard-empty', {}],
-          ['postcss-unprefix', {}],
-          ['postcss-css-variables', {}],
-          ['postcss-preset-env', {}],
-        ],
-      },
-      h5: {
-        plugins: [
-          ['postcss-import', {}],
-          ['tailwindcss', { config: getConfigPath('h5') }],
-          ['postcss-preset-env', {}],
-          ['autoprefixer', {}],
-        ],
-      },
-    };
-    chain.merge({
-      module: {
-        rule: {
-          taroTailwindLoader: {
-            test,
-            use: [
-              {
-                loader: 'postcss-loader',
-                options: {
-                  postcssOptions: postcssConfig[SUPPORTED_MINI_PLATFORMS.includes(CURRENT_PLATFORM) ? 'mini' : 'h5'],
+            h5: {
+                plugins: [
+                    ['postcss-import', {}],
+                    ['tailwindcss', { config: getConfigPath('h5') }],
+                    ['postcss-preset-env', {}],
+                    ['autoprefixer', {}],
+                ],
+            },
+        };
+        chain.merge({
+            module: {
+                rule: {
+                    taroTailwindLoader: {
+                        test,
+                        use: [
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    postcssOptions:
+                                        postcssConfig[
+                                            SUPPORTED_MINI_PLATFORMS.includes(CURRENT_PLATFORM)
+                                                ? 'mini'
+                                                : 'h5'
+                                        ],
+                                },
+                            },
+                        ],
+                    },
                 },
-              },
-            ],
-          },
-        },
-      },
+            },
+        });
     });
-  });
 };
--- src/init.ts
+++ src/init.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 import { IPluginContext } from '@tarojs/service';
 import { HIDDEN_CONFIG_PATH } from './constant';
 
@@ -7,35 +5,46 @@ const fs = require('fs-extra');
 const path = require('path');
 
 export default (ctx: IPluginContext) => {
-  ctx.registerCommand({
-    name: 'tailwind',
-    optionsMap: {
-      '--init': 'generates necessary configs and tailwind.src.css',
-    },
-    synopsisList: ['taro tailwind --init', 'taro tailwind --init weapp,dd,tt,swan'],
-    fn() {
-      const {
-        options: { init },
-      } = ctx.runOpts;
-      const defaultConfig = path.join(__dirname, `../config/mini.config.js`);
-      let generatePlatforms = ['mini', 'h5'];
-      if (init && typeof init === 'string' && init.trim()) {
-        generatePlatforms = init.split(',');
-      }
-      generatePlatforms.map((platform) => {
-        const filePath = `${HIDDEN_CONFIG_PATH}/${platform}.config.js`;
-        const targetFile = path.resolve(filePath);
-        if (fs.existsSync(targetFile)) {
-          console.log(ctx.helper.chalk.redBright(`⚠️ [taro-plugin-tailwind] File ${filePath} exists!`));
-        } else {
-          if (fs.existsSync(path.join(__dirname, `../config/${platform}.config.js`))) {
-            fs.copySync(path.join(__dirname, `../config/${platform}.config.js`), targetFile);
-          } else {
-            fs.copySync(defaultConfig, targetFile);
-          }
-          console.log(ctx.helper.chalk.greenBright(`[taro-plugin-tailwind] File ${filePath} has been created.`));
-        }
-      });
-    },
-  });
+    ctx.registerCommand({
+        name: 'tailwind',
+        optionsMap: {
+            '--init': 'generates necessary configs and tailwind.src.css',
+        },
+        synopsisList: ['taro tailwind --init', 'taro tailwind --init weapp,dd,tt,swan'],
+        fn() {
+            const {
+                options: { init },
+            } = ctx.runOpts;
+            const defaultConfig = path.join(__dirname, `../config/mini.config.js`);
+            let generatePlatforms = ['mini', 'h5'];
+            if (init && typeof init === 'string' && init.trim()) {
+                generatePlatforms = init.split(',');
+            }
+            generatePlatforms.map(platform => {
+                const filePath = `${HIDDEN_CONFIG_PATH}/${platform}.config.js`;
+                const targetFile = path.resolve(filePath);
+                if (fs.existsSync(targetFile)) {
+                    console.log(
+                        ctx.helper.chalk.redBright(
+                            `⚠️ [taro-plugin-tailwind] File ${filePath} exists!`
+                        )
+                    );
+                } else {
+                    if (fs.existsSync(path.join(__dirname, `../config/${platform}.config.js`))) {
+                        fs.copySync(
+                            path.join(__dirname, `../config/${platform}.config.js`),
+                            targetFile
+                        );
+                    } else {
+                        fs.copySync(defaultConfig, targetFile);
+                    }
+                    console.log(
+                        ctx.helper.chalk.greenBright(
+                            `[taro-plugin-tailwind] File ${filePath} has been created.`
+                        )
+                    );
+                }
+            });
+        },
+    });
 };
