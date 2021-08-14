31a05cbbba4e2b28d58f1a90f1583e6c3d8d6000
PCDotFan <i@g4.cx>
2021-08-15 01:19:33 +0800

    #35

--- config/mini.config.js
+++ config/mini.config.js
@@ -1,9 +1,16 @@
+const range = (size) =>
+    Object.fromEntries(
+        [...Array(size).keys()]
+            .slice(1)
+            .map((i) => [`${i}_${size}`, `${(i / size) * 100}%`])
+    );
+
 module.exports = {
     prefixer: false,
-    separator: '_',
+    separator: "_",
     compile: false,
     globalUtility: false,
-    darkMode: 'media',
+    darkMode: "media",
     corePlugins: {
         preflight: false,
         divideColor: false,
@@ -14,50 +21,25 @@ module.exports = {
         placeholderColor: false,
         placeholderOpacity: false,
     },
-    exclude: [
-        /([0-9]{1,}[.][0-9]*)$/,
-    ],
+    exclude: [/([0-9]{1,}[.][0-9]*)$/],
     theme: {
-        width: theme => ({
-            auto: 'auto',
-            ...theme('spacing'),
-            '1_2': '50%',
-            '1_3': '33.333333%',
-            '2_3': '66.666667%',
-            '1_4': '25%',
-            '2_4': '50%',
-            '3_4': '75%',
-            '1_5': '20%',
-            '2_5': '40%',
-            '3_5': '60%',
-            '4_5': '80%',
-            '1_6': '16.666667%',
-            '2_6': '33.333333%',
-            '3_6': '50%',
-            '4_6': '66.666667%',
-            '5_6': '83.333333%',
-            '1_12': '8.333333%',
-            '2_12': '16.666667%',
-            '3_12': '25%',
-            '4_12': '33.333333%',
-            '5_12': '41.666667%',
-            '6_12': '50%',
-            '7_12': '58.333333%',
-            '8_12': '66.666667%',
-            '9_12': '75%',
-            '10_12': '83.333333%',
-            '11_12': '91.666667%',
-            full: '100%',
-            screen: '100vw',
+        width: (theme) => ({
+            auto: "auto",
+            full: "100%",
+            screen: "100vw",
+            ...Object.assign(...[2, 3, 4, 5, 6, 12].map(range)),
+            ...theme("spacing"),
         }),
-        height: theme => ({
-            auto: 'auto',
-            ...theme('width'),
-            screen: '100vh',
+        height: (theme) => ({
+            auto: "auto",
+            full: "100%",
+            screen: "100vh",
+            ...Object.assign(...[2, 3, 4, 5, 6, 12].map(range)),
+            ...theme("spacing"),
         }),
         maxHeight: {
-            full: '100%',
-            screen: '100vh',
+            full: "100%",
+            screen: "100vh",
         },
     },
 };
